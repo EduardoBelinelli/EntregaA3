010dca3dad070bb8894a46b11be8c76dbaa60a67
Eduardo Belinelli <eduardobelinelli10@gmail.com>
2024-12-13 20:17:52 -0300

    entregaA3

---
+++ Aplicacao/script de insercao.sql
@@ -0,0 +1,25 @@
+-- Inserir Produtos
+INSERT INTO Produto (name, price, estoque) VALUES
+('Cadeira', 150.00, 50),
+('Teclado', 250.00, 100),
+('Mouse', 50.00, 200),
+('Monitor', 850.00, 30),
+('Câmera', 200.00, 150),
+('Fone', 180.00, 120),
+('Carregador', 60.00, 80),
+('Impressora', 450.00, 40),
+('Mesa', 350.00, 60),
+('PenDrive', 45.00, 150);
+
+-- Inserir Clientes
+INSERT INTO Cliente (name, cpf) VALUES
+('João', 12345678900),
+('Maria', 98765432100),
+('Carlos', 11122233344),
+('Fernanda', 55566677788),
+('Paula', 99988877766);
+
+-- Inserir Vendedores
+INSERT INTO Vendedor (name, salary) VALUES
+('Lucas', 3500.00),
+('Aline', 4000.00);
